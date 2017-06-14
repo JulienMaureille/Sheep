24c6b13bc4d52eb02eb779733b874fef388ff313
Benjamin Feray <benjamin.feray@etu.unice.fr>
2017-06-14 09:58:00 +0200

    On ne peux plus se connecter avec un pseudo invalide

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,19 +10,33 @@ import {LoginService} from "../../shared/services/login/login.service";
 export class LoginComponent implements OnInit {
 
     public username: string;
+    public defaultpseudo: "anonymous";
 
-    constructor(private loginService : LoginService) {
+    constructor(private loginService: LoginService) {
 
     }
 
-    ngOnInit() { this.username = "anonymous"}
+    ngOnInit() { this.username = this.defaultpseudo; }
 
-    connect(){
-      this.loginService.login(this.username);
+    connectIfValid() {
+      if (this.isValid()) {
+        this.loginService.login(this.username);
+      }
     }
 
-    isConnected(){
+    isConnected() {
       return this.loginService.isConnected();
     }
 
+    isValid() {
+      if (this.username === "") {
+        return false;
+      }
+      for ( let i = 0; i < this.username.length; i++) {
+        if (!(this.username.charCodeAt(i) > 96 && this.username.charCodeAt(i) < 123 )) {
+          return false;
+        }
+      }
+      return true;
+    }
 }
